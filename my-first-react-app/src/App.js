00a7caea6edfd2a8e13cfbadf9b3714c4086ed14
JustWaveThings <justwavethings@proton.me>
2023-04-11 12:09:20 -0400

    second example -- props working with functions

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,17 +1,27 @@
 import './App.css';
+import './meyer_reset.css';
+
 import React, { Component } from 'react';
 import MyComponent from './components/MyComponent';
 
 export class App extends Component {
 	constructor(props) {
 		super(props);
-
+		this.onClickBtn = this.onClickBtn.bind(this);
 		this.state = {};
 	}
+
+	onClickBtn() {
+		console.log('Button clicked');
+	}
+
 	render() {
 		return (
 			<div className="App">
-				<MyComponent title="My First React App" />
+				<MyComponent
+					title="My First React App"
+					onButtonClicked={this.onClickBtn}
+				/>
 			</div>
 		);
 	}
