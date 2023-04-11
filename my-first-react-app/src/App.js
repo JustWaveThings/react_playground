4458423bcfd88ba0231d9bf618f56c66cb7ee913
JustWaveThings <justwavethings@proton.me>
2023-04-11 12:27:24 -0400

    Handle state with a counter

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,14 +1,24 @@
 import './App.css';
-import './meyer_reset.css';
+//import './meyer_reset.css';
 
 import React, { Component } from 'react';
 import MyComponent from './components/MyComponent';
+import CountUp from './components/CountUp';
 
 export class App extends Component {
 	constructor(props) {
 		super(props);
 		this.onClickBtn = this.onClickBtn.bind(this);
-		this.state = {};
+		this.state = {
+			count: 0,
+		};
+		this.countUp = this.countUp.bind(this);
+	}
+
+	countUp() {
+		this.setState({
+			count: this.state.count + 1,
+		});
 	}
 
 	onClickBtn() {
@@ -21,6 +31,11 @@ export class App extends Component {
 				<MyComponent
 					title="My First React App"
 					onButtonClicked={this.onClickBtn}
+				/>
+				<CountUp
+					title="Counting Up"
+					count={this.state.count}
+					onButtonClicked={this.countUp}
 				/>
 			</div>
 		);
--- my-first-react-app/src/components/CountUp.js
+++ my-first-react-app/src/components/CountUp.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 
-class MyComponent extends Component {
+class CountUp extends Component {
 	constructor(props) {
 		super(props);
+
 		this.state = {};
 	}
 	render() {
-		const { title, onButtonClicked } = this.props;
+		const { title, onButtonClicked, count } = this.props;
 		return (
 			<div>
 				<h1>{title}</h1>
 				<button onClick={onButtonClicked}>Click Me</button>
+				<p>{count}</p>
 			</div>
 		);
 	}
 }
 
-export default MyComponent;
+export default CountUp;
